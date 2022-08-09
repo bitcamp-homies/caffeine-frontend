51fd9b7c6b6a9e40bb30910d044a1a7e577adcee
yeonsubak <yeonsubak@gmail.com>
2022-08-09 15:10:52 +0900

    Project initialized: React.js + Typescript, Tailwind CSS, Prettier

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='w-96 h-96 bg-slate-300'>
+      <h1 className='text-rose-600'>Bitcamp Homies: Caffeine Project</h1>
+    </div>
+  );
+}
+
+export default App;
