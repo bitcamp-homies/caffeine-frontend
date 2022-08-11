7512604b5cb8883faec93684e76cf7075aee10d9
yeonsubak <yeonsubak@gmail.com>
2022-08-12 00:28:29 +0900

    #17 Order 내 보조 navBar 추가

--- src/components/order/Order.tsx
+++ src/components/order/Order.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Order = () => {
-  let navigate = useNavigate
-  
-  return (
-    <div>
-      <h2>This is order page.</h2>
-    </div>
-  );
-};
+  let navigate = useNavigate()
 
-export default Order;
+  return (
+    <div
+      id="orderNav"
+      className="
+          sticky flex-row bg-gray-100 border-[1px] border-gray-300
+          md:block md:px-6 md:py-2 lg:px-10 lg:py-3
+    "
+    >
+      <ul className="ml-[5.5rem] list-none self-center">
+        <li className="mr-7 inline-block text-sm">
+          <button onClick={() => { navigate("/menu")}}>All product</button>
+        </li>
+        <li className="mr-7 inline-block text-sm">
+          <button>Featured</button>
+        </li>
+        <li className="mr-7 inline-block text-sm">
+          <button>Previous Orders</button>
+        </li>
+        <li className="mr-7 inline-block text-sm">
+          <button>Favorite Products</button>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Order
