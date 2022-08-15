8449ee01364787d201368d2991cbfac6681a70f1
sudong1004 <suujung1004@gmail.com>
2022-08-15 16:54:27 +0900

    #28-Map-cafeDetail 반응형진행중

--- src/components/stores/SearchPlace.tsx
+++ src/components/stores/SearchPlace.tsx
@@ -53,20 +53,22 @@ const filterdData = data.filter((data:cafeData) => {
 });
 
   return (
-      <div className='flex flex-col basis-2/5 h-128'>
+      <div className='lg:flex lg:flex-col lg:basis-2/5 basis-full h-128'>
         <form className="px-5 inputForm " onSubmit={handleSubmit}>
-          <div className='border-b-2 border-gray-400 inline-block my-6'>
-            <input
-              placeholder="Find a store"
-              onChange={onChange}
-              value={inputText}
-              className="outline-0 w-56"
-            />
-            <button><img className='w-4 h-4 mr-4' src='img/search_icon.png' alt='search_icon'/></button>
+          <div>
+            <div className='border-b-2 border-gray-400 inline-block my-6'>
+              <input
+                placeholder="Find a store"
+                onChange={onChange}
+                value={inputText}
+                className="outline-0 w-56"
+              />
+              <button><img className='w-4 h-4 mr-4' src='img/search_icon.png' alt='search_icon'/></button>
+            </div>
+            <button className='ml-4 border-green-800 border-2 rounded-full text-green-800 w-16 text-center h-8' type="submit">Filter</button>
           </div>
-          <button className='ml-4 border-green-800 border-2 rounded-full text-green-800 w-16 text-center h-8' type="submit">Filter</button>
         </form>
-        <div className='hidden'>
+        <div className='flex lg:hidden'>
         <MapContainer searchPlace={setPlace} />
         </div>
         <CafeList data={filterdData} />
