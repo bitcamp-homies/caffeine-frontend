9286f71d0745f13a713b2fc941bac8c616d5f685
sudong1004 <suujung1004@gmail.com>
2022-08-15 14:18:28 +0900

    #28-Map-cafeDetail 검색기능 구현,Map사라져서 수정해야함

--- src/components/stores/CafeList.tsx
+++ src/components/stores/CafeList.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import axios from 'axios';
 import { ReactComponent as LikeIcon } from './heart-svgrepo-com.svg';
 import { ReactComponent as InfoIcon } from './info-svgrepo-com.svg';
 import { Link } from "react-router-dom";
+import React from 'react';
 
-const CafeList = () => {
+const CafeList = ({data}:any) => {
   type cafeData = {
     insta_account : string,
     cafe_name : string,
@@ -15,24 +14,7 @@ const CafeList = () => {
     about : string,
     subfolder: string
   }
-
-  const [data, setData] = React.useState([]);
   let DetailLink;
-  
-  React.useEffect(
-    () => {
-      axios.get(`http://localhost:8080/cafe/listAll`)
-      .then(
-        (res) => {
-          setData(res.data);
-        }
-      )
-      .catch(
-        (err) => {
-          console.log(err);
-        }
-      )
-    }, [])
   
   return (
 <ul className='w-full h-[440px] overflow-scroll overflow-x-hidden'>
--- src/components/stores/SearchPlace.tsx
+++ src/components/stores/SearchPlace.tsx
@@ -1,23 +1,56 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import CafeList from './CafeList';
 import MapContainer from './MapContainer';
+import axios from 'axios';
+
+type cafeData = {
+  insta_account : string,
+  cafe_name : string,
+  address1 : string,
+  address2 : string,
+  address3 : string,
+  address4 : string,
+  about : string,
+  subfolder: string
+}
 
 type setPlaceType = {
   setPlace : any
 }
 
 const SearchPlace = ({setPlace}: setPlaceType) => {
-const [inputText, setInputText] = useState("");
+const [inputText, setInputText] = useState<string>("");
+const [data, setData] = React.useState<cafeData[]>([]);
 
-const handleSubmit = (event:any) => {
+//데이터 로딩
+React.useEffect(
+  () => {
+    axios.get(`http://localhost:8080/cafe/listAll`)
+    .then(
+      (res) => {
+        setData(res.data);
+      }
+    )
+    .catch(
+      (err) => {
+        console.log(err);
+      }
+    )
+  }, [])
+
+const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
   event.preventDefault();
   setPlace(inputText);
   setInputText("");
 };
 
-const onChange = (event:any) => {
-  setInputText(event.target.value);
-};
+const onChange = (event:React.FormEvent<HTMLInputElement>) => {
+  setInputText(event.currentTarget.value);
+}
+
+const filterdData = data.filter((data:cafeData) => {
+  return data.cafe_name.toLowerCase().includes(inputText.toLowerCase());
+});
 
   return (
       <div className='flex flex-col basis-2/5 h-128'>
@@ -36,7 +69,7 @@ const onChange = (event:any) => {
         <div className='hidden'>
         <MapContainer searchPlace={setPlace} />
         </div>
-        <CafeList />
+        <CafeList data={filterdData} />
       </div>
   );
 };
