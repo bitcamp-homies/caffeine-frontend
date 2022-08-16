6ada85a46219aa2b3584b07ea9aa76098cbfce14
WoongBeeYang <asdz1214@naver.com>
2022-08-16 15:26:42 +0900

    Order Now Page CSS 제작 완료

--- src/components/order/featured/Featured.tsx
+++ src/components/order/featured/Featured.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import FeaturedMenu from './featuredList/FeaturedMenu';
-import OrderNow from './featuredList/OrederNow';
+import React from 'react'
+import { Route } from 'react-router-dom'
+import FeaturedMenu from './featuredList/FeaturedMenu'
+import OrderNow from './featuredList/OrederNow'
 
 const Featured = () => {
-	return (
-		<div>
-			<div className='mt-5 mb-10'>
-        <p className="font-bold sm:text-5xl text-3xl text-center">Summer essentials</p>
-      </div>  
-			<div>
-				<OrderNow/>
-			</div>
-		</div>
-	);
-};
+  return (
+    <div>
+      <div className="mt-5 mb-10">
+        <p className="text-center text-3xl font-bold sm:text-5xl">
+          Summer essentials
+        </p>
+      </div>
+      <div>
+        <OrderNow />
+      </div>
+    </div>
+  )
+}
 
-export default Featured;
+export default Featured
