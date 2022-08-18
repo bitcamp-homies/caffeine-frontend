f5661f2fe010a7f61bd24520de6c4092d46cd18a
sudong1004 <suujung1004@gmail.com>
2022-08-18 18:07:17 +0900

    #43 충돌해결

--- src/components/stores/Carousel.tsx
+++ src/components/stores/Carousel.tsx
@@ -10,48 +10,23 @@ const Carousel = ({data}) => {
     slidesToShow: 1,
     slidesToScroll: 1
   };
-
   
-  const imgPath = data[0].file_path
   const img = data[0].img_file.split(',')
-<<<<<<< HEAD
-  const path = img+data[0].file_path
-  console.log(path)
-=======
-  const filepath = data[0].file_path;
-  console.log(data[0].file_path)
   let test;
-  //'https://storage.cloud.google.com/bitcamp-caffeine.appspot.com'+data[0].file_path+item
-  console.log(test)
->>>>>>> a12f5274639ab4df04a8dc34f7f939d01f09571c
+
   return(
     <div className="carousel">
       <Slider {...settings}>
-
-        <div>
-          <img id="img" className='' />
-        </div>
-        <div className="bg-blue-300">
-          <img src="https://storage.cloud.google.com/bitcamp-caffeine.appspot.com/cafe/seoul/gangnam/a3boutcoffee/gangnam-a3boutcoffee-1.jpg"></img>
-        </div>
-        {
-<<<<<<< HEAD
-          img.map((item,index) => {
-            return(
-              <div key={index}>
-              </div>
-            )
-=======
+      {
           img.map((item) => {
             test = 'https://storage.cloud.google.com/bitcamp-caffeine.appspot.com'+data[0].file_path+item;
             {
             return <div>
-              <img src={test}>
+              <img className="h-96" src={test}>
               </img>
             </div>
             }
 
->>>>>>> a12f5274639ab4df04a8dc34f7f939d01f09571c
           })
         }
       </Slider>
