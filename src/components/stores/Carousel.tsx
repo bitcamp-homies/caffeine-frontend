b85582aed86dbb687e7666549545d29520e57320
sudong1004 <suujung1004@gmail.com>
2022-08-19 11:17:55 +0900

    #43 Location버튼 위치변경

--- src/components/stores/Carousel.tsx
+++ src/components/stores/Carousel.tsx
@@ -1,38 +1,42 @@
 // @ts-nocheck
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-const Carousel = ({data}) => {
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+const Carousel = ({ data }) => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
-  };
-  
-  let img = data[0].img_file.split(',')
-  img = img.filter((element, index) => index < img.length - 1);
-  let test;
+    slidesToScroll: 1,
+  }
 
-  return(
+  let img = data[0].img_file.split(',')
+  img = img.filter((element, index) => index < img.length - 1)
+  let test
+
+  return (
     <div className="carousel">
       <Slider {...settings}>
-      {
-          img.map((item) => {
-            test = 'https://storage.cloud.google.com/bitcamp-caffeine.appspot.com'+data[0].file_path+item;
-            {
-            return <div>
-              <img src={test} style={{ width: "100%", height: "24rem" }}>
-              </img>
-            </div>
-            }
-
-          })
-        }
+        {img.map((item) => {
+          test =
+            'https://storage.cloud.google.com/bitcamp-caffeine.appspot.com' +
+            data[0].file_path +
+            item
+          {
+            return (
+              <div>
+                <img
+                  src={test}
+                  style={{ width: '100%', height: '24rem' }}
+                ></img>
+              </div>
+            )
+          }
+        })}
       </Slider>
     </div>
-  );
+  )
 }
 
-export default Carousel;
+export default Carousel
