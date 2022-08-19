479526d71caa66736793cbd107abd595041b7722
poonghyeok <vpd3302@naver.com>
2022-08-19 14:31:15 +0900

    #66-마커드래그로위치변경구현완료
    1.최초 사용자 위치마커를 드래그하면 사용자위치업데이트
    2.사용자위치업데이트되면서카페리스트도 업데이트
    3.기존의 마커들지우고 새로운 마커들 나타도록 구현
    기타. 그냥 기능구현하기 위해서 더러운코드로작성함....

--- src/components/stores/FindStore.tsx
+++ src/components/stores/FindStore.tsx
@@ -8,37 +8,35 @@ import MapContainer from './MapContainer'
 
 import SearchPlace from './SearchPlace'
 
-  const FindStore = () => {
-  const { cafename } = useParams();
-  const [place, setPlace] = useState('');
-  const [userLocation, setUserLocation] = useState({ lon: 0, lat: 0 });
-  const [cafeList, setCafeList] = useState([]);
-  const [hoverCafe, setHoverCafe] = useState(
-    {
-      "cafe_id": 0,
-      "user_id": 0,
-      "cafe_name": "",
-      "address1": "",
-      "address2": "",
-      "address3": "",
-      "address4": "",
-      "longitude": 0,
-      "latitude": 0,
-      "distance": 0
-    }
-  );
+const FindStore = () => {
+  const { cafename } = useParams()
+  const [place, setPlace] = useState('')
+  const [userLocation, setUserLocation] = useState({ lon: 0, lat: 0 })
+  const [cafeList, setCafeList] = useState([])
+  const [hoverCafe, setHoverCafe] = useState({
+    cafe_id: 0,
+    user_id: 0,
+    cafe_name: '',
+    address1: '',
+    address2: '',
+    address3: '',
+    address4: '',
+    longitude: 0,
+    latitude: 0,
+    distance: 0,
+  })
 
   const getCafeList = (userLocation) => {
     axios
       .get('http://localhost:8080/cafe/listBoundary3000Mybatis', {
         params: {
           userLong: userLocation.lon,
-          userLat: userLocation.lat
+          userLat: userLocation.lat,
         },
       })
       .then((res) => {
-        console.log('FindStore... 3000m 반경이내의 카페...', res.data);
-        setCafeList(res.data); 
+        console.log('FindStore... 3000m 반경이내의 카페...', res.data)
+        setCafeList(res.data)
       })
       .catch((err) => {
         console.log(err)
@@ -51,9 +49,8 @@ import SearchPlace from './SearchPlace'
       navigator.geolocation.getCurrentPosition(function (position) {
         var lat = position.coords.latitude, // 위도
           lon = position.coords.longitude // 경도
-        console.log('FindStore...insertUserLocation....사용자 위치 : ', lon, lat)
         setUserLocation({ lon: lon, lat: lat })
-        getCafeList({ lon: lon, lat: lat });
+        getCafeList({ lon: lon, lat: lat })
       })
     } else {
       // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
@@ -63,23 +60,22 @@ import SearchPlace from './SearchPlace'
 
   //풍혁 0818 : userLocation이랑 cafeList 초기화시켜주기
   useEffect(() => {
-    insertUserLocationAndGetCafeList();
+    insertUserLocationAndGetCafeList()
   }, [])
 
   useEffect(() => {
-    if(userLocation.lon !== 0){
+    if (userLocation.lon !== 0) {
       getCafeList(userLocation)
     }
   }, [userLocation])
 
   return (
-    <div className="ml-4 flex flex-row mt-5">
+    <div className="ml-4 mt-5 flex flex-row">
       <div className="h-128 flex w-96 flex-col">
-        <div>사용자 위치 경도 {userLocation.lon} 위도 : {userLocation.lat} </div>
         {cafename === undefined ? (
-          <SearchPlace 
-            setPlace={setPlace} 
-            setHoverCafe={setHoverCafe} 
+          <SearchPlace
+            setPlace={setPlace}
+            setHoverCafe={setHoverCafe}
             cafeList={cafeList}
           />
         ) : (
@@ -87,14 +83,14 @@ import SearchPlace from './SearchPlace'
         )}
       </div>
       <div className="flex h-full w-full items-stretch">
-        <MapContainer 
+        <MapContainer
           searchPlace={place}
-          userLocation = {userLocation} 
-          setUserLocation={setUserLocation} 
+          userLocation={userLocation}
+          setUserLocation={setUserLocation}
           cafeList={cafeList}
-          setCafeList={setCafeList} 
-          hoverCafe = {hoverCafe}
-          setHoverCafe = {setHoverCafe}
+          setCafeList={setCafeList}
+          hoverCafe={hoverCafe}
+          setHoverCafe={setHoverCafe}
         />
       </div>
     </div>
--- src/components/stores/MapContainer.tsx
+++ src/components/stores/MapContainer.tsx
@@ -1,6 +1,5 @@
 //@ts-nocheck
 
-import cluster from 'cluster'
 import React, { useEffect, useState } from 'react'
 
 let map
@@ -8,81 +7,94 @@ let customOverlay = new kakao.maps.CustomOverlay({
   position: new window.kakao.maps.LatLng(37.54699, 127.09598),
   content: '',
 })
+var cafeMarkers = [];
 
-const MapContainer = (
-  {userLocation, setUserLocation ,cafeList, setCafeList, hoverCafe, setHoverCafe}
- ) => {
-
+const MapContainer = ({
+  userLocation,
+  setUserLocation,
+  cafeList,
+  setCafeList,
+  hoverCafe,
+  setHoverCafe,
+}) => {
+  
   const setMarkerUserLocationOnMap = (userLocation, map) => {
     var locPosition = new window.kakao.maps.LatLng(
       userLocation.lat,
-      userLocation.lon
-    ) 
+      userLocation.lon,
+    )
     // kakao api는 위도 경도 순으로 입력해야하고, POINT TYPE은 경도 위도 순으로 입력해야해서 헷갈리기 쉽다
     var message =
       '<div class="w-[150px] h-9 text-center py-2 px-1 text-[16px] bg-blue-500 text-white font-bold">내 위치</div>' // 인포윈도우에 표시될 내용입니다
 
-    displayMarker(locPosition, message, map);
+    displayMarker(locPosition, message, map)
   }
 
   const displayMarker = (locPosition, message, map) => {
     // 마커를 생성합니다
-    var infowindow;
+    var infowindow
 
     var marker = new window.kakao.maps.Marker({
       map: map,
       position: locPosition,
-      draggable : true,
+      draggable: true,
     })
-    kakao.maps.event.addListener(marker, 'dragstart', function() {
-      infowindow.close();
-      clusterer.clear();
-      marker.setVisible(false);
-    });
+    kakao.maps.event.addListener(marker, 'dragstart', function () {
+      infowindow.close()
+      marker.setVisible(false)
+    })
     //marker가 drag 되었을 때 사용자 좌표 변경 시도..!
-    kakao.maps.event.addListener(marker, 'dragend', function() {
-      alert(marker.getPosition().getLat() + ' '+marker.getPosition().getLng());
-      setUserLocation({lon : marker.getPosition().getLng(), lat:marker.getPosition().getLat()});
-    });
+    kakao.maps.event.addListener(marker, 'dragend', function () {
+
+      setUserLocation({
+        lon: marker.getPosition().getLng(),
+        lat: marker.getPosition().getLat(),
+      })
+    })
 
     var iwContent = message, // 인포윈도우에 표시할 내용
-        iwRemoveable = true
+      iwRemoveable = true
 
-      // 인포윈도우를 생성합니다
-      var infowindow = new window.kakao.maps.InfoWindow({
-        content: iwContent,
-        removable: iwRemoveable,
-      })
+    // 인포윈도우를 생성합니다
+    var infowindow = new window.kakao.maps.InfoWindow({
+      content: iwContent,
+      removable: iwRemoveable,
+    })
 
-      // 인포윈도우를 마커위에 표시합니다
-      infowindow.open(map, marker)
+    // 인포윈도우를 마커위에 표시합니다
+    infowindow.open(map, marker)
 
-      // 지도 중심좌표를 접속위치로 변경합니다
-      map.setCenter(locPosition)
+    // 지도 중심좌표를 접속위치로 변경합니다
+    map.setCenter(locPosition)
   }
 
   const cafeLocPin = (aCafeData, map) => {
-    var coords = new window.kakao.maps.LatLng(aCafeData.latitude, aCafeData.longitude);
+    var coords = new window.kakao.maps.LatLng(
+      aCafeData.latitude,
+      aCafeData.longitude,
+    )
     var marker = new window.kakao.maps.Marker({
       map: map,
       position: coords,
     })
-  };
-  
+
+    cafeMarkers = [...cafeMarkers, marker]
+  }
+
   const setHoverCafeOverlay = (cafeData, map) => {
-    
-    var coords = new window.kakao.maps.LatLng(cafeData.latitude, cafeData.longitude);
+    var coords = new window.kakao.maps.LatLng(
+      cafeData.latitude,
+      cafeData.longitude,
+    )
     var content = `<div class='w-[150px] h-[40px] rounded-full text-center py-2 px-1 bg-green-500 border-none  mb-9 text-white font-semibold text-[12px]'>${cafeData.cafe_name}</div>`
-    
+
     customOverlay = new kakao.maps.CustomOverlay({
       position: coords,
       content: content,
     })
-  
-    customOverlay.setMap(map);
- 
+
+    customOverlay.setMap(map)
   }
-  
 
   React.useEffect(() => {
     // map 렌더링
@@ -96,23 +108,26 @@ const MapContainer = (
   }, [])
 
   React.useEffect(() => {
-    
-    if(cafeList.length > 0){
+    for(var i = 0; i < cafeMarkers.length; i++){
+      cafeMarkers[i].setVisible(false);
+    }
+    console.log('MapContainer...cafeMarkers before...' + cafeMarkers.length)
+    if (cafeList.length > 0) {
       for (var i = 0; i < cafeList.length; i++) {
         cafeLocPin(cafeList[i], map)
       }
-    }    
-  },[cafeList])
+    }
+    console.log('MapContainer...cafeMarkers after...' + cafeMarkers.length)
+  }, [cafeList])
 
   React.useEffect(() => {
-
-    if(userLocation.lon !== 0){
-      setMarkerUserLocationOnMap(userLocation, map);
+    if (userLocation.lon !== 0) {
+      setMarkerUserLocationOnMap(userLocation, map)
     }
   }, [userLocation])
 
   React.useEffect(() => {
-    customOverlay.setMap(null);
+    customOverlay.setMap(null)
     setHoverCafeOverlay(hoverCafe, map)
   }, [hoverCafe])
 
