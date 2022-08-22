3eba899825d429801015f320871c18819bb57642
yeonsubak <yeonsubak@gmail.com>
2022-08-23 03:47:01 +0900

    #79 버거메뉴 버튼 리스트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     <>
       <nav
         id="navBar"
-        className="sticky top-0 z-10 flex flex-row bg-white px-4 py-4 shadow md:px-6 md:py-6 lg:px-10 lg:py-7"
+        className="sticky top-0 z-50 flex flex-row bg-white px-4 py-4 shadow md:px-6 md:py-6 lg:px-10 lg:py-7"
       >
         <Link to="/">
           <Logo />
--- src/components/navbar/BurgerMenu.tsx
+++ src/components/navbar/BurgerMenu.tsx
@@ -4,13 +4,28 @@ interface BurgerProps {
   toggled: boolean
 }
 
-const burgerMenuClass = 'fixed bg-black h-full w-full bg-black-rgba flex flex-row-reverse'
+const burgerMenuClass = 'fixed bg-black h-full w-full bg-black-rgba flex flex-row-reverse z-40'
 
 const BurgerMenu = (props :BurgerProps) => {
   return (
     <div id='burgerMenu' className={props.toggled ? burgerMenuClass : 'hidden'}>
-      <div className='bg-white basis-10/12'>
-        
+      <div className='bg-white basis-10/12 pt-10 px-2'>
+        <ul>
+          <li className='mb-8 px-6 text-2xl'>
+            Order
+          </li>
+          <li className='mb-8 px-6 text-2xl'>
+            Cards
+          </li>
+          <li className='mb-8 px-6 text-2xl'>
+            Gift
+          </li>
+        </ul>
+        <hr className='mx-6 border bg-gray-300 border-gray-300' />
+        <div className='flex flex-row px-5 pt-8'>
+          <button className='text-sm px-3 py-1 mr-4 bg-white border rounded-full border-black'>Sign in</button>
+          <button className='text-sm px-3 py-1 bg-black text-white border rounded-full'>Join now</button>
+        </div>
       </div>
     </div>
   );
