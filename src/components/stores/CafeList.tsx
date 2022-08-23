6c97cee1416a42337e529d696396a12155149614
sudong1004 <suujung1004@gmail.com>
2022-08-23 11:35:23 +0900

    #70 sweetalert 추가

--- src/components/stores/CafeList.tsx
+++ src/components/stores/CafeList.tsx
@@ -4,13 +4,36 @@ import { ReactComponent as InfoIcon } from './svg/info-svgrepo-com.svg'
 import { ReactComponent as FilLikeIcon } from './svg/fill-heart-svgrepo-com.svg'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 const CafeList = ({ filterData, setHoverCafe }) => {
   let DetailLink
-
+  const session = sessionStorage.getItem('Id')
   const [like, setLike] = useState(false)
   const toggleLike = () => {
-    setLike(!like)
+    if (session === null || session === '' || session === undefined) {
+      Swal.fire({
+        title: '정말로 그렇게 하시겠습니까?',
+        text: 'You must sign in to add this store to your favorites.',
+        icon: 'warning',
+
+        showCancelButton: true, // cancel버튼 보이기. 기본은 원래 없음
+        confirmButtonColor: '#3085d6', // confrim 버튼 색깔 지정
+        cancelButtonColor: '#d33', // cancel 버튼 색깔 지정
+        confirmButtonText: '승인', // confirm 버튼 텍스트 지정
+        cancelButtonText: '취소', // cancel 버튼 텍스트 지정
+
+        reverseButtons: true, // 버튼 순서 거꾸로
+      }).then((result) => {
+        // 만약 Promise리턴을 받으면,
+        if (result.isConfirmed) {
+          // 만약 모달창에서 confirm 버튼을 눌렀다면
+
+          Swal.fire('승인이 완료되었습니다.', '화끈하시네요~!', 'success')
+        }
+      })
+    } else setLike(!like)
   }
 
   return (
