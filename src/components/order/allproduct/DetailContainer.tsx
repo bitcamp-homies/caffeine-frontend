3ac038f1d79cea2b817ee37603b476d3807c021a
derekleeck <ckdh1704@gmail.com>
2022-08-24 20:45:35 +0900

    #94 메뉴 연동 Drink 까지 진행

--- src/components/order/allproduct/DetailContainer.tsx
+++ src/components/order/allproduct/DetailContainer.tsx
@@ -6,11 +6,10 @@ const DetailContainer = ({data,menu}) => {
   return (
     <div className="bg-white grid gap-5 grid-col px-6 pt-5 ">
       <div className="pb-4 pt-5 text-bold text-3xl font-bold">Menu</div>
-      {
+      {  
+       
         <div className="text-xl font-bold pb-2">
-          {
-            menu
-          }
+          {menu}
         </div>
       }
       <hr/>
