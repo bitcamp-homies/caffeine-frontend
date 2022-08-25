ab6a3c3e955e106be19c3852d1e0ce1000242e18
poonghyeok <vpd3302@naver.com>
2022-08-25 13:04:20 +0900

    #103-slider생성
    1.materialUI 라이브러리설치
    2.slider 테스트생성완료

--- src/components/stores/ListFilter.tsx
+++ src/components/stores/ListFilter.tsx
@@ -1,43 +1,76 @@
 //@ts-nocheck
-import { ReactComponent as OpenIcon} from './svg/open-svgrepo-com.svg';
-import { ReactComponent as CheckIcon} from './svg/check-svg-maincolor.svg';
-import { ReactComponent as DogIcon} from './svg/dog-svgrepo-com.svg';
-import { ReactComponent as ParkingIcon} from './svg/parking-car-svgrepo-com.svg';
+import { ReactComponent as OpenIcon } from './svg/open-svgrepo-com.svg'
+import { ReactComponent as CheckIcon } from './svg/check-svg-maincolor.svg'
+import { ReactComponent as DogIcon } from './svg/dog-svgrepo-com.svg'
+import { ReactComponent as ParkingIcon } from './svg/parking-car-svgrepo-com.svg'
+import Slider from '@mui/material/Slider';
 
-const ListFilter = ({showFilter, setShowFilter}) => {
+const ListFilter = ({ showFilter, setShowFilter }) => {
+  
+  function valuetext(value) {
+    return `${value}°C`;
+  }
 
   return (
     <div className="h-128 basis-full lg:flex lg:basis-2/5 lg:flex-col">
       <div className="w-full lg:w-[28rem] xl:w-[35rem]">
-        <div className="my-4 px-4"> 
+        <div className="my-4 px-4">
           <span className="text-2xl font-bold tracking-widest">Filters</span>
-          <button className="text-xl font-bold float-right w-10 hover:bg-gray-200 rounded-full text-center" onClick={() => setShowFilter(false)}>X</button>
+          <button
+            className="float-right w-10 rounded-full text-center text-xl font-bold hover:bg-gray-200"
+            onClick={() => setShowFilter(false)}
+          >
+            X
+          </button>
         </div>
-        <div className="text-lg bg-[#F8E5EE] h-12 pl-4 ">
-          <div className="py-2 text-[#3D1308] opacity-70 font-semibold tracking-widest">영업시간</div>
+        <div className="h-12 bg-[#F8E5EE] pl-4 text-lg ">
+          <div className="py-2 font-semibold tracking-widest text-[#3D1308] opacity-70">
+            영업시간
+          </div>
         </div>
-        <div className="text-base h-16 hover:bg-gray-200 pl-4 py-4 text-gray-400">
-          <OpenIcon className='h-8 w-8 inline mx-6 ' fill='#9ca3af'/>
+        <div className="h-16 py-4 pl-4 text-base text-gray-400 hover:bg-gray-200">
+          <OpenIcon className="mx-6 inline h-8 w-8 " fill="#9ca3af" />
           지금 영업중
-          <CheckIcon className='h-6 w-6 inline float-right mx-6' />
+          <CheckIcon className="float-right mx-6 inline h-6 w-6" />
         </div>
 
-        <div className="text-lg bg-[#F8E5EE] h-12 pl-4 ">
-          <div className="py-2 text-[#3D1308] opacity-70 font-semibold tracking-widest">편의사항</div>
+        <div className="h-12 bg-[#F8E5EE] pl-4 text-lg ">
+          <div className="py-2 font-semibold tracking-widest text-[#3D1308] opacity-70">
+            편의사항
+          </div>
         </div>
-        <div className="text-base h-16 hover:bg-gray-200 pl-4 py-4 text-gray-400">
-          <DogIcon className='h-8 w-8 inline mx-6 ' fill='#9ca3af'/>
+        <div className="h-16 py-4 pl-4 text-base text-gray-400 hover:bg-gray-200">
+          <DogIcon className="mx-6 inline h-8 w-8 " fill="#9ca3af" />
           애견동반
-          <CheckIcon className='h-6 w-6 inline float-right mx-6' />
+          <CheckIcon className="float-right mx-6 inline h-6 w-6" />
         </div>
-        <div className="text-base h-16 hover:bg-gray-200 pl-4 py-4 text-gray-400">
-          <ParkingIcon className='h-8 w-8 inline mx-6 ' fill='#9ca3af'/>
+        <div className="h-16 py-4 pl-4 text-base text-gray-400 hover:bg-gray-200">
+          <ParkingIcon className="mx-6 inline h-8 w-8 " fill="#9ca3af" />
           주차가능
-          <CheckIcon className='h-6 w-6 inline float-right mx-6' />
+          <CheckIcon className="float-right mx-6 inline h-6 w-6" />
+        </div>
+
+        <div className="h-12 bg-[#F8E5EE] pl-4 text-lg ">
+          <div className="py-2 font-semibold tracking-widest text-[#3D1308] opacity-70">
+            반경조절
+          </div>
+        </div>
+        <div className='w-96 '>
+          <Slider
+            aria-label="Temperature"
+            defaultValue={1}
+            getAriaValueText={valuetext}
+            valueLabelDisplay="auto"
+            step={1}
+            marks
+            min={1}
+            max={10}
+            color="secondary"
+          />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default ListFilter;
+export default ListFilter
