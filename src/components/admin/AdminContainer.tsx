c2cc6f87329acfde15990a4d54727879fd9903a2
derekleeck <ckdh1704@gmail.com>
2022-08-26 01:27:47 +0900

    #108 header 추가

--- src/components/admin/AdminContainer.tsx
+++ src/components/admin/AdminContainer.tsx
@@ -4,27 +4,43 @@ import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import ContentContainer from './ContentContainer'
 import NaviContainer from './NaviContainer'
+import { useNavigate } from 'react-router-dom'
 
 const AdminContainer = () => {
+  let navigate = useNavigate()
   return (
-    <div className="flex">
-      <NaviContainer />
-      <main className=" min-h-screen w-full">
-        <nav className="flex justify-between bg-white px-10 py-6">
-          {/* 우상단 profile */}
+    /* header */
+    <div>
+      <div
+        id="orderNavBar"
+        className="sticky top-0 border-[1px] border-gray-300 bg-gray-100 text-right md:block md:px-6 md:py-2 lg:px-10 lg:py-3"
+      >
+        <div className="flex flex-row gap-5 justify-center text-xs md:text-sm grid-col">
+          <div className="flex flex-row items-center gap-4">Thumbs ID : {'Im the best'} </div>
+          <div className="flex flex-row items-center gap-4">Thumbs Point : {'20000000000000'}</div>
           <div className="flex flex-row items-center gap-4">
+            
+            <p>profile pic</p>
             <img
               className="w-8 rounded-full"
               src="https://imagez.tmz.com/image/f7/1by1/2021/12/14/f7703994b69d48ca802df55729a2325c_xl.jpg"
               alt="Elon Musk"
             />
-            <p>profile name</p>
           </div>
-          {/* 우상단 profile end*/}
-        </nav>
-        <ContentContainer />
-        <Outlet />
-      </main>
+        </div>
+
+        {/* 우상단 profile */}
+      </div>
+      {/* end of Header */}
+
+      <div className="flex">
+        <NaviContainer />
+        <main className=" min-h-screen w-full">
+          <nav className="flex justify-between bg-white px-10 py-6" />
+          <ContentContainer />
+          <Outlet />
+        </main>
+      </div>
     </div>
   )
 }
--- src/components/admin/ContentContainer.tsx
+++ src/components/admin/ContentContainer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ContentContainer = () => {
   return (
     <div>
-      <div className="w-full ">
+      <div className="w-full px-4">
           <h1 className="text-2xl font-semibold">All Courses</h1>
          {/* 여기 안에서 Like List, OrderedList, PointUsed, Profile 페이지가 돌아야함 */}
         </div>
