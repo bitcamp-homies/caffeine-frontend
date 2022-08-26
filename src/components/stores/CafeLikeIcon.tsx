dc9ef54e835929d428039edcc4a5ef9aaa1d4325
KJK131 <107452935+Jeongqa@users.noreply.github.com>
2022-08-26 18:57:11 +0900

    #144 setAttribute 설정

--- src/components/stores/CafeLikeIcon.tsx
+++ src/components/stores/CafeLikeIcon.tsx
@@ -10,12 +10,25 @@ const CafeLikeIcon = ({ index, item , likeList}) => {
     if (checkthis.id === 'cafe_1' && sessionStorage.getItem("Id") !== null) {
       setLike(!like)
     }
+    const cafe_1 = document.getElementById('cafe_1')
+    cafe_1?.setAttribute('value',item.cafe_id)
+    
+    console.log(cafe_1)
+    const cafe = cafe_1?.getAttribute('value')
+    console.log(cafe)
+    if(like == true){
+      cafe_1?.setAttribute('check','0') //check 안됫을경우
+    }else{
+      cafe_1?.setAttribute('check','1') //check됫을경우
+    }
+    
+    console.log("확인 :" + cafe_1?.getAttribute('check'))
   }
 
   return (
     <div>
       {like ? (
-        <FilLikeIcon onClick={(e) => likeBtn(e.target)} />
+        <FilLikeIcon onClick={(e) => likeBtn(e.target)}/>
       ) : (
         <LikeIcon
           fill="#9F2042"
