e990a265581858242aa9954ba78b6b5b1237bd27
derekleeck <ckdh1704@gmail.com>
2022-08-26 23:02:15 +0900

    #127 다른 브랜치 checkout

--- src/components/order/allproduct/ProductContainer.tsx
+++ src/components/order/allproduct/ProductContainer.tsx
@@ -18,16 +18,14 @@ const ProductContainer = ({ data, cafe_name, img, file_path, img_file }) => {
   if (address !== '') {
     addressfilter = address.filter((item) => item.cafe_id === data[0].cafe_id)
   }
-  console.log(file_path);
-  console.log(img_file);
   return (
     <div className="pt-6">
-      <div className="h-20 w-full rounded-3xl bg-red-800">
-        <div className="flex flex-row justify-center gap-10">
+      <div className="h-24 w-full rounded-2xl bg-red-800">
+        <div className="flex flex-row items-center justify-center gap-10">
           <img
-            className="bg-white h-16 w-16 rounded-full"
+            className="h-20 w-20 rounded-full bg-white"
             src={`https://storage.cloud.google.com/bitcamp-caffeine.appspot.com${img}`}
-            alt="user avatar"
+            alt="image loading.."
             loading="lazy"
           />
 
@@ -36,10 +34,10 @@ const ProductContainer = ({ data, cafe_name, img, file_path, img_file }) => {
           </div>
 
           {/* addres, instagram ID */}
-          <div className="text-sm flex flex-col gap-2">
+          <div className="flex flex-col gap-2 text-sm">
             {/* svg 1 */}
             <div className="flex flex-row items-center gap-1">
-              <svg className="mr-3 h-[24px] w-[24px] overflow-visible fill-current	text-white">
+              <svg className="mr-3 h-[24px] w-[24px] overflow-visible fill-current   text-white">
                 <path d="M11.508 19.22c-.667-.918-1.335-1.888-1.956-2.863-.432-.676-.826-1.33-1.175-1.956-1.037-1.857-1.617-3.357-1.617-4.308 0-2.984 2.297-5.31 5.24-5.31s5.24 2.326 5.24 5.31c0 .95-.58 2.45-1.617 4.31-.35.624-.743 1.28-1.175 1.955-.62.975-1.29 1.945-1.956 2.864-.174.24-.34.465-.492.67-.153-.205-.318-.43-.492-.67zm6.732-9.128c0-3.533-2.743-6.31-6.24-6.31-3.497 0-6.24 2.777-6.24 6.31 0 1.173.628 2.796 1.744 4.796.36.644.763 1.315 1.204 2.007.634.993 1.312 1.98 1.99 2.913.238.327.458.625.656.887l.252.33c.2.256.588.256.788 0l.252-.33c.198-.262.418-.56.656-.887.678-.934 1.356-1.92 1.99-2.913.44-.692.845-1.363 1.204-2.007 1.116-2 1.744-3.623 1.744-4.796z"></path>
                 <path d="M12 13.425c1.592 0 2.88-1.303 2.88-2.908s-1.288-2.91-2.88-2.91c-1.592 0-2.88 1.305-2.88 2.91 0 1.605 1.288 2.908 2.88 2.908zm0-1c-1.037 0-1.88-.853-1.88-1.908 0-1.056.843-1.91 1.88-1.91s1.88.854 1.88 1.91c0 1.055-.843 1.908-1.88 1.908z"></path>
               </svg>
@@ -50,7 +48,7 @@ const ProductContainer = ({ data, cafe_name, img, file_path, img_file }) => {
                   return (
                     <p
                       key={item.cafe_id}
-                      className="text-sm text-white"
+                      className="text-xs text-white"
                       onClick={() => window.open(`${map}${address}`)}
                     >
                       {`${address}${address4}`}
@@ -75,7 +73,7 @@ const ProductContainer = ({ data, cafe_name, img, file_path, img_file }) => {
                   return (
                     <button
                       key={item.cafe_id}
-                      className="truncate text-sm text-white"
+                      className="truncate text-xs text-white"
                       onClick={() =>
                         window.open(`${instagramUrl}${item.insta_account}`)
                       }
