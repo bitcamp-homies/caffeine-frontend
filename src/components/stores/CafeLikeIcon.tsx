bb2e512d15fc30df9c834a76c4aac45dbbca55e6
sudong1004 <suujung1004@gmail.com>
2022-08-28 22:00:00 +0900

    #114 관심목록 뿌리기

--- src/components/stores/CafeLikeIcon.tsx
+++ src/components/stores/CafeLikeIcon.tsx
@@ -1,30 +1,49 @@
 //@ts-nocheck
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ReactComponent as LikeIcon } from './svg/heart-svgrepo-com.svg'
 import { ReactComponent as FilLikeIcon } from './svg/fill-heart-svgrepo-com.svg'
+import axios from 'axios'
 
 const CafeLikeIcon = ({ index, item , likeList}) => {
   const [like, setLike] = useState(false)
 
-  const likeBtn = (checkthis) => {
-    if (checkthis.id === 'cafe_1' && sessionStorage.getItem("Id") !== null) {
-      setLike(!like)
-      
-    }
-    const cafe_1 = document.getElementById('cafe_1')
-    cafe_1?.setAttribute('value',item.cafe_id)
-    
-    console.log(cafe_1)
-    const cafe = cafe_1?.getAttribute('value')
-    console.log(cafe)
-    if(like == true){
-      cafe_1?.setAttribute('check','0') //check 안됫을경우
-    }else{
-      cafe_1?.setAttribute('check','1') //check됫을경우
-    }
-    
-    console.log("확인 :" + cafe_1?.getAttribute('check'))
+  const cafe_check = document.getElementById('cafe_check')
+  cafe_check?.setAttribute('value',item.cafe_id)
+  const cafe = cafe_check?.getAttribute('value')
+  //console.log(cafe_check)
+  //console.log(cafe)
+  
+  if(like == true){
+    cafe_check?.setAttribute('check','0') //check 안됫을경우
+  }else{
+    cafe_check?.setAttribute('check','1') //check됫을경우
   }
+  console.log("확인 :" + cafe_check?.getAttribute('check'))
+
+  const likeBtn = (checkthis) => {
+    if (checkthis.id === 'cafe_check' && sessionStorage.getItem("Id") !== null) {
+      setLike(!like)
+      if(cafe_check?.getAttribute('check') == 1){
+        console.log("cafe update:" + cafe)
+      }else if(cafe_check?.getAttribute('check') == 0){
+        console.log("cafe delete:" + cafe)
+      }
+    }
+  }
+
+  useEffect(() => {
+    {
+      likeList.map((list, index) => {
+        return(
+          <div>
+          {
+            list.cafe_id === item.cafe_id && like ? setLike(true) : setLike(false)
+          }
+          </div>
+        )
+      })
+    }
+  })
 
   return (
     <div>
@@ -35,7 +54,7 @@ const CafeLikeIcon = ({ index, item , likeList}) => {
           fill="#9F2042"
           className="checkbox"
           onClick={(e) => likeBtn(e.target)}
-          id={'cafe_1'}
+          id={'cafe_check'}
         />
       )}
     </div>
