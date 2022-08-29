6abd0712164d51a14fdc55e1c50de48877ddb4b0
poonghyeok <vpd3302@naver.com>
2022-08-29 16:38:44 +0900

    #149-유저테이블크기조절

--- src/components/admin/AdminContainer.tsx
+++ src/components/admin/AdminContainer.tsx
@@ -29,21 +29,33 @@ const AdminContainer = () => {
         id="orderNavBar"
         className="sticky top-0 border-[1px] border-gray-300 bg-gray-100 text-right md:block md:px-6 md:py-2 lg:px-10 lg:py-3"
       >
-        <div className="flex flex-row gap-5 justify-center text-xs md:text-sm grid-col">
-          <div className="hidden md:flex flex-row items-center gap-4 rounded-full bg-gray-200">Thumbs ID : {memberdata.nickname}</div>
-          <div className="hidden sm:flex flex-row items-center gap-4 rounded-full bg-gray-200">Thumbs Point : {20000000}</div>
-          <div className="hidden md:flex flex-row items-center gap-4 rounded-full bg-gray-200">사용자 : {'private'} / {memberdata.user_type}</div>
-          <div className="flex flex-row items-center gap-4 rounded-full bg-gray-200">
-            <p>{memberdata.name}님 안녕하세요!</p>
-            <img
-              className="w-8 rounded-full"
-              src="https://imagez.tmz.com/image/f7/1by1/2021/12/14/f7703994b69d48ca802df55729a2325c_xl.jpg"
-              alt="Elon Musk"
-            />
-          </div>
-          <DropDown />
-          
-        </div>
+        {
+          usertype === 'admin' ?
+          (
+            <div className="w-full text-center text-lg font-semibold ">
+              서비스 관리페이지
+            </div>
+          )
+          :
+          (
+            <div className="flex flex-row gap-5 justify-center text-xs md:text-sm grid-col">
+              <div className="hidden md:flex flex-row items-center gap-4 rounded-full bg-gray-200">Thumbs ID : {memberdata.nickname}</div>
+              <div className="hidden sm:flex flex-row items-center gap-4 rounded-full bg-gray-200">Thumbs Point : {20000000}</div>
+              <div className="hidden md:flex flex-row items-center gap-4 rounded-full bg-gray-200">사용자 : {'private'} / {memberdata.user_type}</div>
+              <div className="flex flex-row items-center gap-4 rounded-full bg-gray-200">
+                <p>{memberdata.name}님 안녕하세요!</p>
+                <img
+                  className="w-8 rounded-full"
+                  src="https://imagez.tmz.com/image/f7/1by1/2021/12/14/f7703994b69d48ca802df55729a2325c_xl.jpg"
+                  alt="Elon Musk"
+                />
+              </div>
+              <DropDown />
+              
+            </div>
+          )
+        }
+        
 
         {/* 우상단 profile */}
       </div>
