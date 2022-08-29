58ef838ef161f7a4a8b94e65039f64ac996653aa
sudong1004 <suujung1004@gmail.com>
2022-08-30 02:40:40 +0900

    수정중

--- src/components/stores/CafeLikeIcon.tsx
+++ src/components/stores/CafeLikeIcon.tsx
@@ -4,7 +4,7 @@ import { ReactComponent as LikeIcon } from './svg/heart-svgrepo-com.svg'
 import { ReactComponent as FilLikeIcon } from './svg/fill-heart-svgrepo-com.svg'
 import axios from 'axios'
 
-const CafeLikeIcon = ({ index, item , likeList}) => {
+const CafeLikeIcon = ({ index, item, likeList }) => {
   const session = sessionStorage.getItem('Id')
   const [like, setLike] = useState(false)
   const [cafe, setCafe] = useState()
@@ -12,24 +12,28 @@ const CafeLikeIcon = ({ index, item , likeList}) => {
 
   const likeBtn = (checkthis) => {
     const cafe_check = document.getElementById('cafe_check')
-    cafe_check?.setAttribute('value',item.cafe_id)
+    cafe_check?.setAttribute('value', item.cafe_id)
     const cafe = cafe_check?.getAttribute('value')
-    
-    if(like == true){
-      cafe_check?.setAttribute('check','0') //check 안됫을경우
-    }else{
-      cafe_check?.setAttribute('check','1') //check됫을경우
-    }
-    console.log("확인 :" + cafe_check?.getAttribute('check'))
 
-    if (cafe !== 0 && checkthis.id === 'cafe_check' && sessionStorage.getItem("Id") !== null) {
+    if (like == true) {
+      cafe_check?.setAttribute('check', '0') //check 안됫을경우
+    } else {
+      cafe_check?.setAttribute('check', '1') //check됫을경우
+    }
+    console.log('확인 :' + cafe_check?.getAttribute('check'))
+
+    if (
+      cafe !== 0 &&
+      checkthis.id === 'cafe_check' &&
+      sessionStorage.getItem('Id') !== null
+    ) {
       setLike(!like)
       setCafe(cafe)
-      if(cafe_check?.getAttribute('check') == 1){
-        console.log("cafe delete:" + cafe)
+      if (cafe_check?.getAttribute('check') == 1) {
+        console.log('cafe delete:' + cafe)
         deleteLikeList(JSON.stringify(data))
-      }else if(cafe_check?.getAttribute('check') == 0){
-        console.log("cafe update:" + cafe)
+      } else if (cafe_check?.getAttribute('check') == 0) {
+        console.log('cafe update:' + cafe)
         updateLikeList(JSON.stringify(data))
       }
     }
@@ -37,16 +41,16 @@ const CafeLikeIcon = ({ index, item , likeList}) => {
   }
 
   const data = {
-    'cafe_id' : cafe,
-    'email' : session
+    cafe_id: cafe,
+    email: session,
   }
-  const qs = require('qs');
+  const qs = require('qs')
 
   const updateLikeList = () => {
     axios({
-      method : 'post',
-      url : `${process.env.REACT_APP_THUMBS_API_ADDRESS}/cafe/updateLikeList`,
-      data : qs.stringify(data),
+      method: 'post',
+      url: `${process.env.REACT_APP_THUMBS_API_ADDRESS}/cafe/updateLikeList`,
+      data: qs.stringify(data),
     })
       .then((res) => {
         console.log('update 성공')
@@ -58,9 +62,9 @@ const CafeLikeIcon = ({ index, item , likeList}) => {
 
   const deleteLikeList = () => {
     axios({
-      method : 'post',
-      url : `${process.env.REACT_APP_THUMBS_API_ADDRESS}/cafe/deleteLikeList`,
-      data : qs.stringify(data),
+      method: 'post',
+      url: `${process.env.REACT_APP_THUMBS_API_ADDRESS}/cafe/deleteLikeList`,
+      data: qs.stringify(data),
     })
       .then((res) => {
         console.log('delete 성공')
@@ -70,23 +74,24 @@ const CafeLikeIcon = ({ index, item , likeList}) => {
       })
   }
 
-  const updateList= ()=> {
-    likeList.map((list)=> {
+  const updateList = () => {
+    likeList.map((list) => {
       setCafeId(list.cafe_id)
       setLike(!like)
-      }
-    )
+    })
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     updateList()
-  },[])
+  }, [])
 
   return (
     <div>
-      { like && cafeId == item.cafe_id
-      ? (
-        <FilLikeIcon className={item.cafe_id} onClick={(e) => likeBtn(e.target)}/>
+      {like && cafeId == item.cafe_id ? (
+        <FilLikeIcon
+          className={item.cafe_id}
+          onClick={(e) => likeBtn(e.target)}
+        />
       ) : (
         <LikeIcon
           fill="#9F2042"
@@ -95,7 +100,6 @@ const CafeLikeIcon = ({ index, item , likeList}) => {
           id={'cafe_check'}
         />
       )}
-      
     </div>
   )
 }
