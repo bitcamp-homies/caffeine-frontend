dfc5ae4847059657e63d3dc9f0fe5ad8cd8aa533
poonghyeok <vpd3302@naver.com>
2022-08-30 18:56:54 +0900

    #164-가입현황그래프초안생성완료

--- src/components/admin/NaviContainerMster.tsx
+++ src/components/admin/NaviContainerMster.tsx
@@ -43,11 +43,49 @@ const NaviContainerMster = () => {
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
               />
             </svg>
             <Link to="UserManageMaster" className="font-semibold">
               전체유저관리
+            </Link>
+          </li>
+          <li className="mt-10 flex cursor-pointer space-x-2 duration-150 hover:text-[#9F2042]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+              />
+            </svg>
+            <Link to="UserManageMaster" className="font-semibold">
+              방문자현황  
+            </Link>
+          </li>
+          <li className="mt-10 flex cursor-pointer space-x-2 duration-150 hover:text-[#9F2042]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
+              />
+            </svg>
+            <Link to="UserAnalytics" className="font-semibold">
+              가입현황
             </Link>
           </li>
           <li className="mt-10 flex cursor-pointer space-x-2 duration-150 hover:text-[#9F2042]">
@@ -65,44 +103,6 @@ const NaviContainerMster = () => {
                 stroke-linejoin="round"
                 stroke-width="2"
                 d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-              />
-            </svg>
-            <Link to="UserManageMaster" className="font-semibold">
-              방문자추이
-            </Link>
-          </li>
-          <li className="mt-10 flex cursor-pointer space-x-2 duration-150 hover:text-[#9F2042]">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-              />
-            </svg>
-            <Link to="UserManageMaster" className="font-semibold">
-              카페등록추이
-            </Link>
-          </li>
-          <li className="mt-10 flex cursor-pointer space-x-2 duration-150 hover:text-[#9F2042]">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"
               />
             </svg>
             <Link to="UserManageMaster" className="font-semibold">
--- src/components/analytics/UserAnalytics.tsx
+++ src/components/analytics/UserAnalytics.tsx
@@ -1,11 +1,201 @@
-import React from 'react';
+//@ts-nocheck
+import React, { useEffect, useState } from 'react'
+import { ResponsiveBar } from '@nivo/bar'
+import axios from 'axios'
+import UserAnalyticDateFilter from './filter/UserAnalyticDateFilter'
+import UserAnalyticTypeFilter from './filter/UserAnalyticTypeFilter'
+import LineGraph from './LineGraph'
 
 const UserAnalytics = () => {
+  const handle = {
+    barClick: (data: any) => {
+      console.log(data)
+    },
+
+    legendClick: (data: any) => {
+      console.log(data)
+    },
+  }
+
+  const [userFilter, setUserFilter] = useState('business');
+  const [dateFilter, setDateFilter] = useState('day');
+
+  const [analyticData, setAnalyticData] = useState([]);
+  useEffect(() => {
+    axios
+      .get(
+        `${process.env.REACT_APP_THUMBS_API_ADDRESS}/user/getUserAnalyticMybatis`,
+        {
+          params :
+          {
+            user_type : userFilter,
+            date_type : dateFilter,
+          }
+        },
+      )
+      .then((res) => {
+        setAnalyticData(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })  
+  },[userFilter, dateFilter])
+
   return (
-    <div>
-      
+    // chart height이 100%이기 때문이 chart를 덮는 마크업 요소에 height 설정
+    <div className='w-full h-[500px] m-auto'>
+      <div>
+        {userFilter} {dateFilter}
+        <UserAnalyticTypeFilter setUserFilter={setUserFilter}/>  
+        <UserAnalyticDateFilter setDateFilter={setDateFilter}/>
+      </div>
+      <ResponsiveBar
+        /**
+         * chart에 사용될 데이터
+         * {
+              "num": 8,
+              "month": 0,
+              "create_date": "2022-06-01"
+            },
+         */
+        data={analyticData}
+        /**
+         * chart에 보여질 데이터 key (측정되는 값)
+         */
+        keys={['num']}
+        /**
+         * keys들을 그룹화하는 index key (분류하는 값)
+         */
+        indexBy={
+          dateFilter === 'day'?
+          'create_date'
+          :
+          'month'
+        }
+        /**
+         * chart margin
+         */
+        margin={{ top: 50, right: 130, bottom: 50, left: 100 }}
+        /**
+         * chart padding (bar간 간격)
+         */
+        padding={0.3}
+        /**
+         * chart 색상
+         */
+        colors={['#9F2042']} // 커스터하여 사용할 때
+        // colors={{ scheme: 'nivo' }} // nivo에서 제공해주는 색상 조합 사용할 때
+        /**
+         * color 적용 방식
+         */
+        colorBy="id" // 색상을 keys 요소들에 각각 적용
+        // colorBy="indexValue" // indexBy로 묵인 인덱스별로 각각 적용
+        theme={{
+          /**
+           * label style (bar에 표현되는 글씨)
+           */
+          labels: {
+            text: {
+              fontSize: 14,
+              fill: '#000000',
+            },
+          },
+          /**
+           * legend style (default로 우측 하단에 있는 색상별 key 표시)
+           */
+          legends: {
+            text: {
+              fontSize: 12,
+              fill: '#000000',
+            },
+          },
+          axis: {
+            /**
+             * axis legend style (bottom, left에 있는 글씨)
+             */
+            legend: {
+              text: {
+                fontSize: 20,
+                fill: '#000000',
+              },
+            },
+            /**
+             * axis ticks style (bottom, left에 있는 값)
+             */
+            ticks: {
+              text: {
+                fontSize: 16,
+                fill: '#000000',
+              },
+            },
+          },
+        }}
+        /**
+         * axis bottom 설정
+         */
+        axisBottom={{
+          tickSize: 5, // 값 설명하기 위해 튀어나오는 점 크기
+          tickPadding: 5, // tick padding
+          tickRotation: 0, // tick 기울기
+          legend: '날짜', // bottom 글씨
+          legendPosition: 'middle', // 글씨 위치
+          legendOffset: 40, // 글씨와 chart간 간격
+        }}
+        /**
+         * axis left 설정
+         */
+        axisLeft={{
+          tickSize: 5, // 값 설명하기 위해 튀어나오는 점 크기
+          tickPadding: 5, // tick padding
+          tickRotation: 0, // tick 기울기
+          legend: '등록수', // left 글씨
+          legendPosition: 'middle', // 글씨 위치
+          legendOffset: -60, // 글씨와 chart간 간격
+        }}
+        /**
+         * label 안보이게 할 기준 width
+         */
+        labelSkipWidth={36}
+        /**
+         * label 안보이게 할 기준 height
+         */
+        labelSkipHeight={12}
+        /**
+         * bar 클릭 이벤트
+         */
+        onClick={handle.barClick}
+        /**
+         * legend 설정 (default로 우측 하단에 있는 색상별 key 표시)
+         */
+        legends={[
+          {
+            dataFrom: 'keys', // 보일 데이터 형태
+            anchor: 'bottom-right', // 위치
+            direction: 'column', // item 그려지는 방향
+            justify: false, // 글씨, 색상간 간격 justify 적용 여부
+            translateX: 120, // chart와 X 간격
+            translateY: 0, // chart와 Y 간격
+            itemsSpacing: 2, // item간 간격
+            itemWidth: 100, // item width
+            itemHeight: 20, // item height
+            itemDirection: 'left-to-right', // item 내부에 그려지는 방향
+            itemOpacity: 0.85, // item opacity
+            symbolSize: 20, // symbol (색상 표기) 크기
+            effects: [
+              {
+                // 추가 효과 설정 (hover하면 item opacity 1로 변경)
+                on: 'hover',
+                style: {
+                  itemOpacity: 1,
+                },
+              },
+            ],
+            onClick: handle.legendClick, // legend 클릭 이벤트
+          },
+        ]}
+      />
     </div>
-  );
-};
+  )
+}
 
 export default UserAnalytics;
