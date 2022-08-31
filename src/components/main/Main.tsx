6b2198fa4f4b126bb0294e33e446817cb1333f4b
poonghyeok <vpd3302@naver.com>
2022-08-31 14:50:48 +0900

    poong_main_시간변경인사말반영

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -4,6 +4,18 @@ import CafeSwipeContainer from './CafeSwipeContainer'
 import FooterContainer from './FooterContainer'
 
 const Main = () => {
+  const getTimeWord = () => {
+    const nowHour = new Date().getHours()
+    if (nowHour >= 4 && nowHour < 12) {
+      return 'morning'
+    } else if (nowHour >= 12 && nowHour < 18) {
+      return 'afternoon'
+    } else if (nowHour >= 18 && nowHour < 21) {
+      return 'evening'
+    } else {
+      return 'night'
+    }
+  }
   return (
     <div id="main" className="flex flex-col lg:flex-row">
       <div
@@ -11,7 +23,7 @@ const Main = () => {
         className="hidden basis-2/5 border-r-[1px] bg-white md:flex"
       >
         <div className="border-b-[1px] px-4 py-4 text-left text-3xl font-bold text-zinc-800 md:border-b-0 md:px-6 md:py-6 lg:mt-40 lg:px-10 lg:py-7 lg:text-center">
-          Good morning ☕
+          Good {getTimeWord()} ☕
         </div>
       </div>
       <div id="rightDiv" className="basis-3/5">
